e9b903a5815b61f01b44987b370a280748d71e47
wuzhantu <326862309@qq.com>
2020-03-24 11:12:50 +0800

    加点注释而已

--- Example/TuWaterWave/WUAppDelegate.m
+++ Example/TuWaterWave/WUAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    //storyboard初始化
     return YES;
 }
 
